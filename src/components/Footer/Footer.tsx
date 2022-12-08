beb83587c01126010d0131223731b977d4b675ab
pelumi <polalekan@gmail.com>
2022-12-08 07:27:51 +0100

    footer completed

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -13,24 +13,69 @@ const Footer = () => {
                 <Col md="3" sm="6">
                     <Logo />
                     <p>Lorem ipsum dolor sit amet consectetur. </p>
-                    <div className="footer_row">
-                        <a href={facebook}>
-                            <img loading='lazy' alt="who we are" className='left img-fluid'
-                                src={require(`assets/images/svgs/LinkedIn.svg`).default}
-                            />
-                        </a>
-                    </div>
+                    <Row>
+                        <Col md="auto">
+                            <a href={linkedin}>
+                                <img loading='lazy' alt="Our LinkendIn page" className='left img-fluid'
+                                    src={require(`assets/images/svgs/LinkedIn.svg`).default}
+                                />
+                            </a>
+                        </Col>
+                        <Col md="auto">
+                            <a href={facebook}>
+                                <img loading='lazy' alt="Our Facebook page" className='left img-fluid'
+                                    src={require(`assets/images/svgs/Facebook.svg`).default}
+                                />
+                            </a>
+                        </Col>
+                        <Col md="auto">
+                            <a href={instagram}>
+                                <img loading='lazy' alt="Our Instagram page" className='left img-fluid'
+                                    src={require(`assets/images/svgs/Instagram.svg`).default}
+                                />
+                            </a>
+                        </Col>
+                        <Col md="auto">
+                            <a href={twitter}>
+                                <img loading='lazy' alt="Our Twittwe page" className='left img-fluid'
+                                    src={require(`assets/images/svgs/Twitter.svg`).default}
+                                />
+                            </a>
+                        </Col>
+                    </Row>
                 </Col>
                 <Col md="3" sm="6">
                     <h5>Quick Links</h5>
-
+                    <p>Who we are</p>
+                    <p>Our Team</p>
+                    <p>Contact Us</p>
                 </Col>
                 <Col md="3" sm="6">
                     <h5>Our Solutions</h5>
-
+                    <p>Data Security</p>
+                    <p>Business Growth</p>
+                    <p>Quality Assurance</p>
                 </Col>
                 <Col md="3" sm="6">
                     <h5>Contact Information</h5>
+                    <p>
+                        <img loading='lazy' alt="Address" className='left img-fluid'
+                            src={require(`assets/images/svgs/Location.svg`).default}
+                        /> &nbsp;
+                        <strong>USA Office 1:</strong> 230 Houston Texas, Atlanta Georgia. USA.
+                    </p>
+                    <p>
+                        <img loading='lazy' alt="Telephone number" className='left img-fluid'
+                            src={require(`assets/images/svgs/Telephone.svg`).default}
+                        /> &nbsp;
+                        0091 333 444 555
+                    </p>
+                    <p>
+                    <img loading='lazy' alt="email" className='left img-fluid'
+                            src={require(`assets/images/svgs/Message.svg`).default}
+                        /> &nbsp;
+                        info@gyb.com
+                    </p>
 
                 </Col>
             </Row>

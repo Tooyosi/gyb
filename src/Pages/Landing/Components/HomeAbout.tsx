9c810e3dab94df539d8bd9c4b43f08e741f46d39
pelumi <polalekan@gmail.com>
2022-12-22 04:00:49 +0100

    fixed design issues

--- src/Pages/Landing/Components/HomeAbout.tsx
+++ src/Pages/Landing/Components/HomeAbout.tsx
@@ -14,7 +14,7 @@ const HomeAbout = () => {
         </Col>
         <Col md="6">
           <div className="text_col">
-            <h3 className='small-header'>WHO WE ARE</h3>
+            <h3 className='small-header mt-3 mt-md-0'>WHO WE ARE</h3>
             <h2 className='main-header'>Lorem ipsum dolor sit amet consectetur. Duis vestibulum faucibus porta facilisis faucibus.</h2>
             <p>Lorem ipsum dolor sit amet consectetur. Urna sagittis mattis venenatis augue. Elementum augue in massa hendrerit enim non. Blandit sit ac adipiscing ultricies ut viverra nunc rutrum. Tincidunt nullam viverra ultricies interdum lectus lectus. Ultricies consectetur et magna urna aliquet sed. </p>
             <p>Condimentum leo maecenas et sit odio imperdiet blandit. Amet duis ante id morbi. Odio faucibus vel nec enim lorem aliquam sed. Sed proin.</p>
